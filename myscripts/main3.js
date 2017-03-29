d41f4d5522804ad2a107b6990a45f5ad5baeec89
Tommy Dang <nhontuan@gmail.com>
2017-03-29 00:18:22 -0500

    update index

--- myscripts/main3.js
+++ myscripts/main3.js
@@ -6,15 +6,39 @@
  * OF THIS SOFTWARE OR ITS FITNESS FOR ANY PARTICULAR PURPOSE.
  */
 
-function selectHistogram(cut2){
+var selectedCut = 1;
+
+function setCut(){
+    var selectedvalue = $('#sdropdown').val();
+    if(selectedvalue==="optimized"){
+
+    }else{
+         selectedCut = +selectedvalue-1;
+    }
+    selectHistogram();
+}
+
+function selectHistogram(){
     for (var c=0; c<numCut;c++) {
-        if (c==cut2){
+        if (c==selectedCut){
             svg.selectAll(".histogram"+c).style("fill-opacity", 1)
                 .style("stroke-opacity", 1);
+            for (var m = 1; m < numMonth; m++) {
+                var nodes = [];
+                if (graphByMonths[m][c]!=undefined){
+                    nodes= graphByMonths[m][c].nodes;
+                }
+                var links = [];
+                if (graphByMonths[m][c]!=undefined){
+                    links= graphByMonths[m][c].links;
+                }
+                updateSubLayout(nodes, links, m);
+            }
         }
         else{
             svg.selectAll(".histogram"+c).style("fill-opacity", 0.1)
-                .style("stroke-opacity", 0.4);
+                .style("stroke-opacity", 0.3);
         }
     }
 }
+
